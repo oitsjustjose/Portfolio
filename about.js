eae226bc964141b1f3369d76b024e835c2a9c6e4
oitsjustjose <stovallj1995@gmail.com>
2018-03-27 07:11:25 -0500

    --

--- about.js
+++ about.js
@@ -1,23 +1,33 @@
 $(function() {
+    $('a.sccc-2018').click(function() {
+        $('.ui.modal.sccc-2018').modal('show');
+        moveDown();
+    });
+
+    $('a.sccc-2017').click(function() {
+        $('.ui.modal.sccc-2017').modal('show');
+        moveDown();
+    });
+
     $('a.create-a-thon').click(function() {
         $('.ui.modal.create-a-thon').modal('show');
         moveDown();
-    })
+    });
 
     $('a.fantastic_five').click(function() {
         $('.ui.modal.fantastic_five').modal('show');
         moveDown();
-    })
+    });
 
     $('a.nissan').click(function() {
         $('.ui.modal.nissan').modal('show');
         moveDown();
-    })
+    });
 
     $('a.modding').click(function() {
         $('.ui.modal.modding').modal('show');
         moveDown();
-    })
+    });
 });
 
 function moveDown() {
