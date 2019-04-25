f71242a65063dc8421d4b5d9dbe1634980ad78e6
oitsjustjose <stovallj1995@gmail.com>
2019-04-25 12:59:44 -0400

    Updoots

--- about.js
+++ about.js
@@ -1,35 +1,45 @@
-$(function() {
-    $('a.sccc-2018').click(function() {
+$(function () {
+    $('a.cuip-2019').click(function () {
+        $('.ui.modal.cuip-2019').modal({
+            onHide: function () {
+                $(".ui.embed").embed("destroy");
+            }
+        }).modal('show');
+        $(".ui.embed").embed();
+        moveDown();
+    });
+
+    $('a.sccc-2018').click(function () {
         $('.ui.modal.sccc-2018').modal('show');
         moveDown();
     });
 
-    $('a.sccc-2017').click(function() {
+    $('a.sccc-2017').click(function () {
         $('.ui.modal.sccc-2017').modal('show');
         moveDown();
     });
 
-    $('a.create-a-thon').click(function() {
+    $('a.create-a-thon').click(function () {
         $('.ui.modal.create-a-thon').modal('show');
         moveDown();
     });
 
-    $('a.fantastic_five').click(function() {
+    $('a.fantastic_five').click(function () {
         $('.ui.modal.fantastic_five').modal('show');
         moveDown();
     });
 
-    $('a.nissan').click(function() {
+    $('a.nissan').click(function () {
         $('.ui.modal.nissan').modal('show');
         moveDown();
     });
 
-    $('a.modding').click(function() {
+    $('a.modding').click(function () {
         $('.ui.modal.modding').modal('show');
         moveDown();
     });
 });
 
 function moveDown() {
-  footer.style.bottom = (footer.style.bottom-14)+'px';
+    footer.style.bottom = (footer.style.bottom - 14) + 'px';
 }
