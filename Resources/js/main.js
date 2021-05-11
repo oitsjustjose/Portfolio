f642103fd3466b9347ecb07de95e0ea6a60458fd
oitsjustjose <stovallj1995@gmail.com>
2021-05-11 01:45:31 -0500

    Lookin for a job 😊

--- Resources/js/main.js
+++ Resources/js/main.js
@@ -1,4 +1,7 @@
 const ColorModes = { Dark: 'dark', Light: 'light' };
+// Yes this is dumb, but it's an easy way to handle this.
+const lookingForJob = true && !window.localStorage.getItem('hasSeenJobModal');
+
 // Set the current color mode to the default display mode
 let currentColorMode = window.matchMedia('(prefers-color-scheme: dark)').matches
     ? ColorModes.Dark : ColorModes.Light;
@@ -123,9 +126,71 @@ const bootstrapNavbar = () => {
     }
 };
 
+const bootstrapJobModal = () => {
+    if (!lookingForJob) {
+        return;
+    }
+
+    const modal = document.createElement('div');
+    modal.className = 'modal fade';
+    modal.id = 'hireMeModal';
+    modal.addEventListener('hide.bs.modal', () => {
+        window.localStorage.setItem('hasSeenJobModal', true);
+    });
+
+    const modalWr = document.createElement('div');
+    modalWr.className = 'modal-dialog modal-lg modal-fullscreen-lg-down modal-dialog-centered';
+
+    const modalContent = document.createElement('div');
+    modalContent.className = 'modal-content';
+
+    const modalHeader = document.createElement('div');
+    modalHeader.className = 'modal-header';
+    modalHeader.innerHTML = `
+        <h2 class='modal-title'>I'm Looking for a Job!</h2>
+    `;
+
+    const modalBody = document.createElement('div');
+    modalBody.className = 'modal-body';
+    modalBody.innerHTML = `
+    <p>
+        Hi there! My name is Jose Stovall and I'm looking for a new job! I am currently
+        contracted through Ernst & Young as a Data Modeler, but my contract ends in
+        late July so I'm preemptively trying to situate a future, permanent position
+        with a new company for after my contract ends!
+    </p>
+
+    <p>
+        Currently I am located just outside of Nashville, TN but I am looking to
+        relocate to the Seattle WA area so I am currently looking for positions both
+        in Nashville and in the Seattle area. I am set up to work remotely but I'm 
+        always glad to go in to the office as I'm fully COVID-19 vaccinated!
+    </p>
+
+    <p>
+        You can see my knowledge/interests/expertise here on this main page as well as 
+        on the Portfolio page (found using the dropdown above). Additionally my work
+        experience can be found using the dropdown, and you can contact me through LinkedIn
+        in the menu or <a href="mailto:stovallj1995@gmail.com">via email</a>!
+    </p>
+
+    <sub>You should only see this once</sub>
+    `;
+
+    modalContent.appendChild(modalHeader);
+    modalContent.appendChild(modalBody);
+    modalWr.appendChild(modalContent);
+    modal.appendChild(modalWr);
+    document.body.appendChild(modal);
+
+    const modalObj = new bootstrap.Modal(modal);
+    modalObj.show();
+};
+
 window.addEventListener('load', () => {
     initColorScheme();
     bootstrapNavbar();
+    bootstrapJobModal();
 
     document.getElementById('displayModeSwitch').addEventListener('click', () => {
         currentColorMode = currentColorMode === ColorModes.Dark ? ColorModes.Light : ColorModes.Dark;
